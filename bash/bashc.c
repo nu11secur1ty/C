99eb6c31901bb0d920ffaf2b1ffd60c2db184b0b
nu11secur1ty <venvaropt@gmail.com>
2021-05-11 17:16:16 +0300

    Create bashc.c

---
+++ bash/bashc.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+  // Check Google //four packages
+  char* command = "ping -c4 google.com";
+	command = "ping -c4 google.com";
+
+    system(command);
+    return 0;
+}
