fb4b1d2bc82a2c197ef4a553b04ea66a12a4f068
nu11secur1ty <venvaropt@gmail.com>
2021-05-11 17:21:18 +0300

    Update bashc.c

--- bash/bashc.c
+++ bash/bashc.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	char* command = "ping -c4 google.com";//
+	char* command = "ping -c4 google.com";//you can change it with your domain =)
 	command = "ping -c4 google.com";
 
     system(command);
