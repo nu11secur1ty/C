14dc9d7d9b5625760019031dfc6b5fb5409f41c7
nu11secur1ty <venvaropt@gmail.com>
2021-05-11 17:40:57 +0300

    Create maimunacall.c

---
+++ bash/call_script/maimunacall.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	char* command = "bash maimuna.sh";//you can change it with your domain =)
+	command = "bash maimuna.sh";
+
+    system(command);
+    return 0;
+}
